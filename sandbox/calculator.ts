3dcebfe58759d02157f60be53246c624cb14d0c7
Risto Koverola <risto.koverola@eficode.com>
2022-07-22 17:57:25 +0300

    Save progress until 9.1

---
+++ sandbox/calculator.ts
@@ -0,0 +1,26 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = number | string;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Cannot divide by zero!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Unknown operation');
+  }
+};
+
+try {
+  console.log(calculator(1, 5, 'divide'));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
