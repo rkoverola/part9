9237b960bba2a17ba28f72be9cdd1c647ad1070d
Risto Koverola <risto.koverola@eficode.com>
2022-07-25 15:18:02 +0300

    Solve 9.4

---
+++ exercises/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3004;
+
+app.listen(PORT, () => {
+  console.log(`Starting server on port ${PORT}`);
+});
