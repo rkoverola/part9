042e7e1c922c74264c82da7c4445cdc096a7902b
Risto Koverola <risto.koverola@eficode.com>
2022-07-27 16:16:27 +0300

    Save progress until 9.14

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Welcome = ({ name }: { name: string }) => {
+  return <h1>Hello, {name}</h1>;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const element = <Welcome name="Sara" />;
+ReactDOM.render(element, document.getElementById('root'));
